8485f212f8bb2cc0af40b9aedce8a4e0527feddd
Muhammad-Zain01 <zainmemon010@gmail.com>
2023-12-10 21:30:59 +0500

    Update data.ts

--- src/common/data.ts
+++ src/common/data.ts
@@ -1,7 +1,7 @@
 const data = [
     'Promotion an ourselves up otherwise my. High what each snug rich far yet easy. In companions inhabiting mr principles at insensible do. Heard their sex hoped enjoy vexed child fo exed child fo exed child for.',
     'Traveling became almost extinct during the pandemic.Dolores wouldn\'t have eaten the meal if she known what it actually was. She wrote him a long letter, but he didn\'t read it.',
-    'Sometimes it is better to just walk away from things and go back to them later when you’re in a better frame of mind. It isn\'t difficult to do a handstand if you just stand on your hands.'
+    'Sometimes it is better to just walk away from things and go back to them later when you\'re in a better frame of mind. It isn\'t difficult to do a handstand if you just stand on your hands.'
 ]
 
 export const getText = () => {
