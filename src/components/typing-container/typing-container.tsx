26bd67c3b8eac8afb4c5070903481adc4333f7ad
Muhammad-Zain01 <zainmemon010@gmail.com>
2023-12-10 19:53:59 +0500

    Update typing-container.tsx

--- src/components/typing-container/typing-container.tsx
+++ src/components/typing-container/typing-container.tsx
@@ -1,29 +1,41 @@
+'use client'
 import classes from './typing-container.module.css'
 import React from 'react'
-
+import { useRef } from 'react'
 type ComponentProps = {
     data: string[];
     index: number;
     status: number;
 }
 const statuses = ['current-key', 'green', 'red']
-const TypingContainer: React.FC<ComponentProps> = ({ data, index, status }) => {
+const TypingContainer: React.FC<ComponentProps> = ({ data, index, status, onKeyPress }) => {
+    const inputRef = useRef()
     return (
-        <div className={classes['typing-box']}  >
-            <div className={classes.div}>
-                {
-                    data.map((item, idx) => {
-                        return (
-                            <span
-                                key={idx}
-                                className={`${idx === index ? classes[statuses[status]] : ""}  ${idx < index ? classes['green'] : ""}`}>
-                                {item}
-                            </span>
-                        )
-                    })
-                }
+        <>
+            <div className={classes['typing-box']} onClick={() => inputRef.current.focus()}  >
+                <div className={classes.div}>
+                    {
+                        data.map((item, idx) => {
+                            return (
+                                <span
+                                    key={idx}
+                                    className={`${idx === index ? classes[statuses[status]] : ""}  ${idx < index ? classes['green'] : ""}`}>
+                                    {item}
+                                </span>
+                            )
+                        })
+                    }
+                </div>
+
             </div>
-        </div>
+            <input
+                style={{ opacity: 0 }}
+                ref={inputRef}
+                onKeyDown={onKeyPress}
+                type="text"
+            />
+        </>
+
     )
 }
 
