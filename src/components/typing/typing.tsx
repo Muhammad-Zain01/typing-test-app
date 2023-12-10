824fb2a0cbf66d1be7ddaf4e971cee4623de2e04
Muhammad-Zain01 <zainmemon010@gmail.com>
2023-12-10 15:56:15 +0500

    Update typing.tsx

--- src/components/typing/typing.tsx
+++ src/components/typing/typing.tsx
@@ -1,14 +1,31 @@
+'use client'
 import classes from './typing.module.css'
 import TypingContainer from '../typing-container/typing-container';
 import TypingCards from '../typing-cards/typing-cards';
+import { useEffect, useState } from 'react';
+import { data } from '@/text';
+import useKeyPress from '@/hooks/useKeyPress';
 const Typing = () => {
+    const words = data.split('')
+    const [currentIndex, setCurrentIndex] = useState(0)
+    const [currentKey, setCurrentKey] = useState("")
+    const [status, setStatus] = useState(0);
+    useEffect(() => {
+        if (currentKey === words[currentIndex]) {
+            setCurrentIndex(currentIndex + 1)
+        }
+    }, [currentKey])
+
+    const ExecuteType = (key: string) => setCurrentKey(key)
+    useKeyPress(ExecuteType);
+
     return (
         <>
             <div className={classes['parameter-container']}>
                 <TypingCards />
             </div>
             <div className={classes['typing-container']}>
-                <TypingContainer />
+                <TypingContainer data={words} index={currentIndex} />
             </div>
         </>
     )
