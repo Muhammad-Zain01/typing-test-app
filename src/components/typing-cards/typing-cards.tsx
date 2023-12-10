51b79a9d7df8e29a531f8dea2c67447cecdf84ae
Muhammad-Zain01 <zainmemon010@gmail.com>
2023-12-10 15:56:05 +0500

    Update typing-cards.tsx

--- src/components/typing-cards/typing-cards.tsx
+++ src/components/typing-cards/typing-cards.tsx
@@ -6,14 +6,25 @@ const TypingCards = () => {
     return (
         <div className={classes['card-wrapper']}>
             <div className={classes.timer}>
-                <CircularProgress value={40} size='120px' thickness='4px' color='#ffd000'>
-                    <CircularProgressLabel>40</CircularProgressLabel>
+                <CircularProgress
+                    style={{ marginRight: 40 }}
+                    value={40}
+                    size='120px'
+                    thickness='4px'
+                    color='#ffd000'
+                >
+                    <CircularProgressLabel>
+                        <span className={classes['timer-label']}>
+                            <span> 60 </span>
+                            <span className={classes.second}>SECONDS</span>
+                        </span>
+                    </CircularProgressLabel>
                 </CircularProgress>
             </div>
             <div className={classes.cards}>
-                <Card />
-                <Card />
-                <Card />
+                <Card label='words/min' />
+                <Card label='chars/min' />
+                <Card label='% accuracy' />
             </div>
         </div>
     )
