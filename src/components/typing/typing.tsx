78dcac2da9a7bca2ac980061c3a8477b357f8dd0
Muhammad-Zain01 <zainmemon010@gmail.com>
2023-12-10 21:31:14 +0500

    Update typing.tsx

--- src/components/typing/typing.tsx
+++ src/components/typing/typing.tsx
@@ -5,16 +5,16 @@ import TypingCards from '../typing-cards/typing-cards';
 import { useState, useRef } from 'react';
 import { getText } from '@/common/data';
 import TypingResult from '../typing-result/typing-result';
+import { useEffect } from 'react';
+import { TIMER } from '@/common/settings';
 const Typing = () => {
-    const words = data.split('')
-
     const typeStrokes = useRef(0)
     const keyStrokes = useRef(1)
     const intervalId = useRef(null);
     const calcIntervalId = useRef(null);
     const timeRef = useRef(1);
 
-    const [text, setText] = useState<string[]>(getText())
+    const [text, setText] = useState([])
     const [currentIndex, setCurrentIndex] = useState(0)
     const [wpm, setwpm] = useState(0)
     const [cpm, setcpm] = useState(0)
@@ -32,25 +32,26 @@ const Typing = () => {
 
     const reset = () => {
         typeStrokes.current = 0
+        keyStrokes.current = 1
         timeRef.current = 1;
-        setText(data2.split(''))
+        setText(getText())
         setTimer(0)
         setCurrentIndex(0)
         setStatus(0)
-        setCurrentKey('')
         setwpm(0)
         setcpm(0)
         setAccuracy(0)
+        intervalId.current = null
+        calcIntervalId.current = null
     }
-
     const initTimer = () => {
         if (!intervalId.current) {
             intervalId.current = setInterval(() => {
                 setTimer((prevTimer) => {
-                    if (prevTimer === 60) {
+                    if (prevTimer === TIMER) {
                         clearInterval(intervalId.current);
                         setResultModal(true)
-                    } else if (prevTimer < 60) {
+                    } else if (prevTimer < TIMER) {
                         timeRef.current = timeRef.current + 1
                         return prevTimer + 1
                     }
@@ -65,7 +66,6 @@ const Typing = () => {
             calcIntervalId.current = setInterval(() => {
                 const typed = typeStrokes.current;
                 const elaspedTime = timeRef.current;
-                console.log(typed, elaspedTime)
                 const wpm = Math.round((typed / 5) * 60 / elaspedTime)
                 const cpm = Math.round((typed / elaspedTime) * 60 / 1)
                 const accuracy = Math.round((typed / keyStrokes.current) * 100)
@@ -84,6 +84,7 @@ const Typing = () => {
             keyStrokes.current = keyStrokes.current + 1
             initTimer()
             calcParams()
+
             if (key === text[currentIndex]) {
                 setCurrentIndex(currentIndex + 1)
                 typeStrokes.current = typeStrokes.current + 1
@@ -91,12 +92,14 @@ const Typing = () => {
             } else {
                 setStatus(2)
             }
-            if (words.length === (currentIndex + 1)) {
+            if (text.length === (currentIndex + 1)) {
                 changeText()
             }
         }
     }
-
+    useEffect(() => {
+        setText(getText());
+    },[])
     return (
         <>
             <div className={classes['parameter-container']}>
