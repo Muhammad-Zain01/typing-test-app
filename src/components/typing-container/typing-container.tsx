8e82ae177bf6aecbf2748aa84876bbc1bb62a3c5
Muhammad-Zain01 <zainmemon010@gmail.com>
2023-12-10 11:23:34 +0500

    Project File Setup

---
+++ src/components/typing-container/typing-container.tsx
@@ -0,0 +1,9 @@
+const TypingContainer = () => {
+    return (
+        <div>
+            Typing Container
+        </div>
+    )
+}
+
+export default TypingContainer
---
+++ src/components/typing/typing.tsx
@@ -0,0 +1,16 @@
+import classes from './typing.module.css'
+
+const Typing = () => {
+    return (
+        <>
+            <div className={classes['parameter-container']}>
+                --- HERE PARAMETERS CHECKER ---
+            </div>
+            <div className={classes['typing-container']}>
+                Main Typing Div
+            </div>
+        </>
+    )
+}
+
+export default Typing;
