31d22ac6c95c3d09cbf9e142e419a8fb7d56768a
Muhammad-Zain01 <zainmemon010@gmail.com>
2023-12-10 18:05:57 +0500

    Update typing-cards.tsx

--- src/components/typing-cards/typing-cards.tsx
+++ src/components/typing-cards/typing-cards.tsx
@@ -1,30 +1,41 @@
 'use client'
+import React, { useState } from 'react'
 import classes from './typing-cards.module.css'
-import { CircularProgress, CircularProgressLabel } from '@chakra-ui/react'
 import Card from './card'
-const TypingCards = () => {
+import { CircularProgress, CircularProgressLabel } from '@chakra-ui/react'
+
+type ComponentProps = {
+    timer: number;
+    wpm: number;
+    cpm: number;
+    accuracy: number;
+}
+const TypingCards: React.FC<ComponentProps> = ({ timer, wpm, cpm, accuracy }) => {
+    // const [percentage, setPercentage] = useState<number>(100)
+    let time = 60;
+    let percentage = 100 - ((100 / time) * timer)
     return (
         <div className={classes['card-wrapper']}>
             <div className={classes.timer}>
                 <CircularProgress
                     style={{ marginRight: 40 }}
-                    value={40}
-                    size='120px'
+                    value={percentage}
+                    size='150px'
                     thickness='4px'
                     color='#ffd000'
                 >
                     <CircularProgressLabel>
                         <span className={classes['timer-label']}>
-                            <span> 60 </span>
+                            <span> {time - timer} </span>
                             <span className={classes.second}>SECONDS</span>
                         </span>
                     </CircularProgressLabel>
                 </CircularProgress>
             </div>
             <div className={classes.cards}>
-                <Card label='words/min' />
-                <Card label='chars/min' />
-                <Card label='% accuracy' />
+                <Card label='words/min' value={wpm} />
+                <Card label='chars/min' value={cpm} />
+                <Card label='% accuracy' value={accuracy} />
             </div>
         </div>
     )
