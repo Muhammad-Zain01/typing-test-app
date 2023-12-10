6b3e21192b250d4145c81d1fbb3db3723242c37a
Muhammad-Zain01 <zainmemon010@gmail.com>
2023-12-10 18:06:07 +0500

    Update text.ts

--- src/text.ts
+++ src/text.ts
@@ -1,7 +1,2 @@
-export const data = `Promotion an ourselves up otherwise my. High what each snug rich far yet easy. In companions inhabiting mr principles at insensible do. Heard their sex hoped enjoy vexed child for. Prosperous so occasional assistance it discovered especially no. Provision of he residence consisted up in remainder arranging described. Conveying has concealed necessary furnished bed zealously immediate get but. Terminated as middletons or by instrument. Bred do four so your felt with. No shameless principle dependent household do.
-For who thoroughly her boy estimating conviction. Removed demands expense account in outward tedious do. Particular way thoroughly unaffected projection favourable mrs can projecting own. Thirty it matter enable become admire in giving. See resolved goodness felicity shy civility domestic had but. Drawings offended yet answered jennings perceive laughing six did far.
-`
-
-export const getText = () => {
-    return text.split(' ')
-}
+export const data = 'Promotion an ourselves up otherwise my. High what each snug rich far yet easy. In companions inhabiting mr principles at insensible do. Heard their sex hoped enjoy vexed child fo exed child fo exed child for.'
+export const data2 = 'High what each snug rich far yet easy. In companions inhabiting mr principles at insensible do. Heard their sex hoped enjoy vexed child fo exed child fo exed child for Promotion an ourselves up otherwise my.'
