a9c209bb80372912ab17c0a51ab01c31c5c6c59c
zmemon <zainmemon010@gmail.com>
2023-12-11 17:51:48 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { Provider } from './chakra-provider'
-
+import { TypingProvider } from '@/context/typing-context'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -19,7 +19,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className} suppressHydrationWarning={true}>
         <Provider>
-          {children}
+          <TypingProvider>
+            {children}
+          </TypingProvider>
         </Provider>
       </body>
     </html>
