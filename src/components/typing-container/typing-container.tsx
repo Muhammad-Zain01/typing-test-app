12d8397a97e8c277a9b3ffd3d7172db54455aac2
zmemon <zainmemon010@gmail.com>
2023-12-11 17:52:03 +0500

    Update typing-container.tsx

--- src/components/typing-container/typing-container.tsx
+++ src/components/typing-container/typing-container.tsx
@@ -2,25 +2,26 @@
 import classes from './typing-container.module.css'
 import React, { useState } from 'react'
 import { useRef } from 'react'
+import useTypingContext from '@/hooks/useTypingContext'
 type ComponentProps = {
-    data: string[];
-    index: number;
-    status: number;
+    onKeyPress: (key: any) => void
 }
-const statuses = ['current-key', 'green', 'red']
-const TypingContainer: React.FC<ComponentProps> = ({ data, index, status, onKeyPress }) => {
-    const inputRef = useRef()
+const TypingContainer: React.FC<ComponentProps> = ({ onKeyPress }) => {
+    const { currentParagraph, currentIndex, currentStatus } = useTypingContext()
     const [isFocused, setIsFocused] = useState(false)
+    const inputRef = useRef()
+    const Status = ['current-key', 'green', 'red']
+
     return (
         <>
             <div className={classes['typing-box']} onClick={() => inputRef.current.focus()}  >
                 <div className={classes.div}>
                     {
-                        data.map((item, idx) => {
+                        currentParagraph.map((item, idx) => {
                             return (
                                 <span
                                     key={idx}
-                                    className={`${isFocused && idx === index ? classes[statuses[status]] : ""}  ${idx < index ? classes['green'] : ""}`}>
+                                    className={`${isFocused && idx === currentIndex ? classes[Status[currentStatus]] : ""}  ${idx < currentIndex ? classes['green'] : ""}`}>
                                     {item}
                                 </span>
                             )
