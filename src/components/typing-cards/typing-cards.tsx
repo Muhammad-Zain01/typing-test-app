8189cceac0e7453dbf712a666a146343cc662d29
Muhammad-Zain01 <zainmemon010@gmail.com>
2023-12-12 21:19:22 +0500

    Update typing-cards.tsx

--- src/components/typing-cards/typing-cards.tsx
+++ src/components/typing-cards/typing-cards.tsx
@@ -1,32 +1,33 @@
 import React from 'react'
 import classes from './typing-cards.module.css'
 import Card from './card'
-// import { CircularProgress, CircularProgressLabel } from '@chakra-ui/react'
 import { TIMER } from '@/common/settings'
 import useTypingContext from '@/hooks/useTypingContext'
+import { Progress } from 'antd'
 const TypingCards: React.FC = () => {
     const { currentTimer, wpm, cpm, accuracy } = useTypingContext()
     let time = TIMER;
     let percentage = 100 - ((100 / TIMER) * currentTimer)
-    
+
     return (
         <div className={classes['card-wrapper']}>
             <div className={classes.timer}>
-                {/* <CircularProgress
-                    style={{ marginRight: 40 }}
-                    value={percentage}
-                    size='150px'
-                    thickness='4px'
-                    color='#ffd000'
-                >
-                    <CircularProgressLabel>
-                        <span className={classes['timer-label']}>
-                            <span> {time - currentTimer} </span>
-                            <span className={classes.second}>SECONDS</span>
-                        </span>
-                    </CircularProgressLabel>
-                </CircularProgress> */}
+                <Progress
+                    type="circle"
+                    percent={percentage}
+                    size={120}
+                    strokeWidth={4}
+                    strokeColor={'#ffd000'}
+                    style={{ backgroundColor: "white", borderRadius: '100%' }}
+                    format={
+                        () => (
+                            <span className={classes['timer-label']}>
+                                <span> {time - currentTimer} </span>
+                                <span className={classes.second}>seconds</span>
+                            </span>
+                        )} />
             </div>
+            <br />
             <div className={classes.cards}>
                 <Card label='words/min' value={wpm} />
                 <Card label='chars/min' value={cpm} />
