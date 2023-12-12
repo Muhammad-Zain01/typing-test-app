8820b3ade3bcda13772055369d5c7608845dea7a
Muhammad-Zain01 <zainmemon010@gmail.com>
2023-12-12 21:36:41 +0500

    Update setting.tsx

--- src/components/setting/setting.tsx
+++ src/components/setting/setting.tsx
@@ -1,17 +1,46 @@
-import { SettingOutlined } from '@ant-design/icons'
+import { CheckOutlined, CloseOutlined, SettingOutlined } from '@ant-design/icons'
 import classes from './setting.module.css'
-import { Popover } from 'antd';
+import { Popover, Select, Switch } from 'antd';
 const Settings = () => {
     const settigns = (
-        <div>
-            Settings
+        <div style={{ width: '200px', padding: '10px 0px' }}>
+            <div>
+                <label htmlFor='time' style={{ margin: "0px 2px" }}>
+                    Time
+                </label>
+                <div style={{ margin: "5px 0px" }}>
+                    <Select
+                        id='time'
+                        defaultValue={60}
+                        style={{ width: '100%' }}
+                        options={[
+                            { value: 60, label: '1 Minute' },
+                            { value: 120, label: '2 Minute' },
+                            { value: 180, label: '3 Minute' },
+                            { value: 300, label: '5 Minute' },
+                        ]}
+                    />
+                </div>
+            </div>
+            <div style={{ display: 'flex', marginTop: 10, justifyContent: 'space-between', alignItems: 'center' }}>
+                <label htmlFor='time' style={{ margin: "0px 2px" }}>
+                    Sound
+                </label>
+                <div style={{ marginTop: 5 }}>
+                    <Switch
+                        checkedChildren={<CheckOutlined />}
+                        unCheckedChildren={<CloseOutlined />}
+                        defaultValue={true}
+                    />
+                </div>
+            </div>
         </div>
     )
     return (
         <div className={classes['setting-container']}>
             <Popover
                 className='test'
-                placement="topLeft"
+                placement="leftTop"
                 title="Settings"
                 content={settigns}
                 trigger="click"
