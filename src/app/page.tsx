0f7eeba6c556ab4863a1e8a26f646d63b1581a08
Muhammad-Zain01 <zainmemon010@gmail.com>
2023-12-12 21:19:06 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import classes from './page.module.css'
 import Typing from '@/components/typing/typing'
+import Settings from '@/components/setting/setting'
 export default function Home() {
   return (
     <main className={classes.main}>
@@ -14,6 +15,7 @@ export default function Home() {
         </div>
         <Typing />
       </div>
+      <Settings />
     </main>
   )
 }
